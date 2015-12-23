2ae83d6d6215b807e04d257e3087cdc362d3066a
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-23 13:33:57 +0100

    Add grunt
    Rearranged assets and vendor

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    "use strict";
+
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // jsHint
+        // Run jsHint syntax checking on all necessary .js files
+        jshint: {
+            all: ['Gruntfile.js', 'assets/**/*.js'],
+        },
+
+        // bower
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'assets/js/vendor',
+                    layout: 'byComponent'
+                }
+            }
+        },
+
+        // browsersync
+        browserSync: {
+            dev: {
+                files: {
+                    src: [
+                        'assets/css/*.css',
+                        'assets/img/*',
+                        'assets/js/*.js',
+                        '**/*.html'
+                    ],
+                },
+                options: {
+                    watchTask: false,
+                    server: './'
+                }
+            }
+        }
+    });
+
+
+    // register tasks
+    // init:dev
+    grunt.registerTask('init:dev', ['bower']);
+    // build:dev
+
+    // default
+    grunt.registerTask('default', ['browserSync:dev']);
+};
