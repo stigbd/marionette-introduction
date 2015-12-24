d2875af4f3472fbdfbbd7e0e6c36060456e62b10
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-24 16:02:42 +0100

    Moving the Contact Entities

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -1,2 +1,28 @@
 ContactManager.module("Entities", function(Entities,
-ContactManager, Backbone, Marionette, $, _) {});
+ContactManager, Backbone, Marionette, $, _) {
+    Entities.Contact = Backbone.Model.extend({
+        defaults: {
+            firstName: "",
+            phoneNumber: "No phone number!"
+        }
+    });
+
+    Entities.ContactCollection = Backbone.Collection.extend({
+        model: Entities.Contact,
+
+        comparator: function(a, b) {
+            var aFirstName = a.get("firstName");
+            var bFirstName = b.get("firstName");
+            if(aFirstName === bFirstName) {
+                var aLastName = a.get("lastName");
+                var bLastName = b.get("lastName");
+                if(aLastName === bLastName) { return 0; }
+                if(aLastName < bLastName) { return -1;}
+                else { return 1;}
+            }
+            else {
+                if(aFirstName < bFirstName) { return -1; }
+            }
+        }
+    });
+});
