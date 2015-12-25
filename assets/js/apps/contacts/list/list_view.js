4b6d22a7ad50f3b78f3bd40bf58c930be8956399
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-25 21:32:27 +0100

    Animating the Removed ItemView

--- assets/js/apps/contacts/list/list_view.js
+++ assets/js/apps/contacts/list/list_view.js
@@ -20,7 +20,15 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager,
             deleteClicked: function(e){
                 e.stopPropagation();
                 this.trigger("contact:delete", this.model);
+            },
+
+            remove: function(){
+                var self = this;
+                this.$el.fadeOut(function() {
+                    Marionette.ItemView.prototype.remove.call(self);
+                });
             }
+
         });
 
         List.Contacts = Marionette.CompositeView.extend({
