6013b2147fbfcea0edc3567350ce49cf0ee9804b
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-26 16:07:07 +0100

    Routing Helpers

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
+/*jshint -W030 */
 var ContactManager = new Marionette.Application();
+
+ContactManager.navigate = function(route, options) {
+    options || (options = {});
+    Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function(){
+    return Backbone.history.fragment;
+};
 
 ContactManager.on("before:start", function(){
     var RegionContainer = Marionette.LayoutView.extend({
@@ -15,5 +25,10 @@ ContactManager.on("start", function() {
 
     if(Backbone.history){
         Backbone.history.start();
+        if(this.getCurrentRoute() === ""){
+            this.navigate("contacts");
+
+            ContactManager.ContactsApp.List.Controller.listContacts();
+        }
     }
 });
