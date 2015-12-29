3644679ac246149369618857ceb119c6ab19893c
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-29 21:34:51 +0100

    Refactor out to common views.js

--- assets/js/apps/contacts/edit/edit_view.js
+++ assets/js/apps/contacts/edit/edit_view.js
@@ -1,52 +1,10 @@
 ContactManager.module("ContactsApp.Edit", function(Edit, ContactManager,
     Backbone, Marionette, $, _){
-        Edit.Contact = Marionette.ItemView.extend({
-            template: "#contact-form",
+        Edit.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
 
             initialize: function(){
                 this.title = "Edit " + this.model.get("firstName");
                 this.title += " " + this.model.get("lastName");
-            },
-
-            events: {
-                "click button.js-submit": "submitClicked"
-            },
-
-            submitClicked: function(e){
-                e.preventDefault();
-                var data = Backbone.Syphon.serialize(this);
-                this.trigger("form:submit", data);
-            },
-
-            onRender: function(){
-                if( ! this.options.asModal) {
-                    var $title = $("<h1>", { text: this.title });
-                    this.$el.prepend($title);
-                }
-            },
-
-            onFormDataInvalid: function(errors){
-                var $view = this.$el;
-
-                var clearFormErrors = function() {
-                    var $form = $view.find("form");
-                    $form.find(".help-block-error").each(function() {
-                        $(this).remove();
-                    });
-                    $form.find(".form-group.error").each(function() {
-                        $(this).removeClass("error");
-                    });
-                };
-
-                var markErrors = function(value, key){
-                    var $controlGroup = $view.find("#contact-" +
-                    key).parent();
-                    var $errorEl = $("<span>", {class: "help-block-error",
-                    text: value});
-                    $controlGroup.append($errorEl).addClass("error");
-                };
-                clearFormErrors();
-                _.each(errors, markErrors);
             }
         });
     });
