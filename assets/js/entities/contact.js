86714717cdfb7f3d933adfcdbd029141207db5e9
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-29 20:33:36 +0100

    Replace sort-function with simple comparator

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -32,20 +32,7 @@ ContactManager.module("Entities", function(Entities,
             url: "contacts",
             model: Entities.Contact,
 
-            comparator: function(a, b) {
-                var aFirstName = a.get("firstName");
-                var bFirstName = b.get("firstName");
-                if(aFirstName === bFirstName) {
-                    var aLastName = a.get("lastName");
-                    var bLastName = b.get("lastName");
-                    if(aLastName === bLastName) { return 0; }
-                    if(aLastName < bLastName) { return -1;}
-                    else { return 1;}
-                }
-                else {
-                    if(aFirstName < bFirstName) { return -1; }
-                }
-            },
+            comparator: "firstName",
             localStorage: new Backbone.LocalStorage("Contacts")
         });
 
