4dfe3599d53686b161299baae7dc83cf4911ef41
Stig B. Dørmænen <stigbd@gmail.com>
2015-12-29 20:47:38 +0100

    Subdividing Complex Views with Layouts

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -7,10 +7,19 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager,
 
                 var fetchingContacts =
                 ContactManager.request("contact:entities");
+
+                var contactsListLayout = new List.Layout();
+                var contactsListPanel = new List.Panel();
+
                 $.when(fetchingContacts).done(function(contacts){
 
                     var contactsListView = new List.Contacts({
                         collection:contacts
+                    });
+
+                    contactsListLayout.on("show", function(){
+                        contactsListLayout.panelRegion.show(contactsListPanel);
+                        contactsListLayout.contactsRegion.show(contactsListView);
                     });
 
                     contactsListView.on("childview:contact:show",
@@ -52,7 +61,7 @@ ContactManager.module("ContactsApp.List", function(List, ContactManager,
                     });
 
 
-                    ContactManager.regions.main.show(contactsListView);
+                    ContactManager.regions.main.show(contactsListLayout);
                 });
             }
         };
